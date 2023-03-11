553660ce69a6f2115f06f128737faef3d300a189
schalk <sv@easybooking.at>
2023-03-11 12:35:56 +0100

    start chapter 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
+	"github.com/schalkwv/snippetbox/internal/models"
 	"net/http"
 	"strconv"
 )
@@ -13,27 +14,39 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/pages/home.html",
-		"./ui/html/partials/nav.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.errorLog.Print(err.Error())
-		//http.Error(w, "Internal Server Error", 500)
 		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		//app.errorLog.Print(err.Error())
-		//http.Error(w, "Internal Server Error", 500)
-		app.serverError(w, err)
-		return
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	/*
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/pages/home.html",
+			"./ui/html/partials/nav.html",
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.errorLog.Print(err.Error())
+			//http.Error(w, "Internal Server Error", 500)
+			app.serverError(w, err)
+			return
+		}
+
+		err = ts.ExecuteTemplate(w, "base", nil)
+		if err != nil {
+			//app.errorLog.Print(err.Error())
+			//http.Error(w, "Internal Server Error", 500)
+			app.serverError(w, err)
+			return
+		}
+	*/
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -43,7 +56,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -41,10 +42,71 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 // Get This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	return nil, nil
+
+	s := &Snippet{}
+
+	err := m.DB.QueryRow("SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return s, nil
+	//stmt := `SELECT id, title, content, created, expires FROM snippets
+	//WHERE expires > UTC_TIMESTAMP() AND id = ?`
+	//
+	//s := &Snippet{}
+	//
+	//row := m.DB.QueryRow(stmt, id)
+	//
+	//// Use row.Scan() to copy the values from each field in sql.Row to the
+	//// corresponding field in the Snippet struct. Notice that the arguments
+	//// to row.Scan are *pointers* to the place you want to copy the data into,
+	//// and the number of arguments must be exactly the same as the number of
+	//// columns returned by your statement.
+	//err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	//if err != nil {
+	//	// If the query returns no rows, then row.Scan() will return a
+	//	// sql.ErrNoRows error. We use the errors.Is() function check for that
+	//	// error specifically, and return our own ErrNoRecord error instead
+	//	if errors.Is(err, sql.ErrNoRows) {
+	//		return nil, ErrNoRecord
+	//	} else {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//// If everything went OK then return the Snippet object.
+	//return s, nil
 }
 
 // Latest This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
 }
