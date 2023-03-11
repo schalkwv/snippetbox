37448252a241bba7ba8e17a23234131d6d79c3db
schalk <sv@easybooking.at>
2023-03-11 12:07:52 +0100

    insert post working

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -54,5 +54,15 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
---
+++ internal/models/snippets.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert This will insert a new snippet into the database.
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result to get the ID of our
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
+	return int(id), nil
+}
+
+// Get This will return a specific snippet based on its id.
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+// Latest This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
+}
